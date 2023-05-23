a279a90dbe13f51195a470b63c893d98a2f61a93
sianrobarts <131804764+sianrobarts@users.noreply.github.com>
2023-05-23 14:45:15 +0100

    Add files via upload

---
+++ guided_clustering_tutorial.R
@@ -0,0 +1,23 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+
+# Load the PBMC dataset
+pbmc.data <- Read10X(data.dir = "~/Documents/Dissertation/filtered_gene_bc_matrices_hg19")
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
+
+pbmc
+
+# Lets examine a few genes in the first thirty cells
+pbmc.data[c("CD3D", "TCL1A", "MS4A1"), 1:30]
+## 3 x 30 sparse Matrix of class "dgCMatrix"
+
+# The [[ operator can add columns to object metadata. This is a great place to stash QC stats
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+# Show QC metrics for the first 5 cells
+head(pbmc@meta.data, 5)
+
+# Visualize QC metrics as a violin plot
+VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+
+
