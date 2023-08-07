ec1260d8b2057e6214771a9d48620b236d45022a
sianrobarts <sian.g.robarts@gmail.com>
2023-08-07 16:10:33 +0100

    add a new feature

--- converting data to seurat object.R
+++ converting data to seurat object.R
@@ -11,39 +11,67 @@ pbmc <- NormalizeData(pbmc) #normalising the data according to seurat vignete
 lookatmatrix <- pbmc
 
 pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000) #identifying key genes
+
+# Scale the data
+pbmc <- ScaleData(pbmc)
+# The expression values of each gene in each cell are now scaled to have zero mean and unit variance
+
+# visualize the top 10 variable features
 top10 <- head(VariableFeatures(pbmc), 10)
 plot1 <- VariableFeaturePlot(pbmc)
-plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE, xnudge = 0, ynudge = 0, )
 plot1 + plot2
-
 
 #perfoming linear dimensional reduction
 pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc)) 
-VizDimLoadings(pbmc, dims = 1:5, reduction = "pca") #this gave the list of gene plot
-DimPlot(pbmc, reduction = "pca", raster=F) #this gave the scatterlooking plot.
+VizDimLoadings(pbmc, dims = 1:5, reduction = "pca") # visualize gene along the first five principal components and how they contribute +ve/-ve to PC.  
+
+# show the expression distribution of the top 10 variable features across different cell populations or groups.
+VlnPlot( pbmc, features = top10, cols = NULL, pt.size = NULL,
+  idents = NULL,
+  sort = FALSE,
+  group.by = NULL,
+  split.by = NULL,
+  adjust = 1,
+  y.max = NULL,
+  same.y.lims = FALSE,
+  log = FALSE,
+  ncol = NULL,
+  slot = "data",
+  split.plot = FALSE,
+  stack = FALSE,
+  combine = TRUE,
+  fill.by = "feature",
+  flip = FALSE,
+  raster = NULL
+)
+
+# visualize cells in reduced dimensions (PCA space)
+DimPlot(pbmc, reduction = "pca", raster=FALSE) #this gave the scatterlooking plot.
 print(pbmc[["pca"]], dims = 1:5, nfeatures = 5) #Examine and visualize PCA results another way: lsit
 
-DimHeatmap(pbmc, dims = 1, cells = 500, balanced = TRUE) #was unable to run this...
 
-#trying to run jackstraw plot
-pbmc <- JackStraw(pbmc, num.replicate = 100) #run later babe. Update, did run later now doesnt work idk why
+# heatmap focusing on a principal component. Both cells and genes are sorted by their principal component scores. 
+DimHeatmap(pbmc, dims = 1:10, nfeatures = 30, cells = 500, reduction = "pca", balanced = TRUE) 
+# Allows for nice visualization of sources of heterogeneity in the dataset.
+
+
+#trying to run jackstraw plot and elbow plot
+pbmc <- JackStraw(pbmc, num.replicate = 100) 
 pbmc <- ScoreJackStraw(pbmc, dims = 1:20)
-JackStrawPlot(
-  pbmc,
-  dims = 1:20,
-  cols = NULL,
-  reduction = "pca",
-  xmax = 0.1,
-  ymax = 0.3
-) 
+JackStrawPlot(pbmc, dims = 1:20, cols = NULL, reduction = "pca", xmax = 0.1, ymax = 0.3) 
 
+# Elbow plot used to visualize the explained variance by each principal component and helps in determining the optimal number of dimensions to retain for downstream analysis.
 ElbowPlot(pbmc)
 
 #cluster the cells 
 pbmc <- FindNeighbors(pbmc, dims = 1:10)
 pbmc <- FindClusters(pbmc, resolution = 0.5)
+# Look at cluster IDs of the first 5 cells
+head(Idents(pbmc), 5)
 
-#Run non-linear dimensional reduction (UMAP/tSNE)
+
+#Run non-linear dimensional reduction (UMAP)
 pbmc <- RunUMAP(pbmc, dims = 1:15)
 DimPlot(pbmc, reduction = "umap")
 
--- single_cell_guided_clustering.R
+++
@@ -1,6 +0,0 @@
-library(Seurat)
-library(SeuratData)
-library(SeuratDisk)
-
-
-resting_memory.data <- Read10X_h5(data.dir = "/Users/Sian/Documents/research_project/raw_data")
